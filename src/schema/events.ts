d96314c236814909258bf8260d131eb8ddc7aef1
Taras Bratyshchenko <taras.bratyshchenko@gmail.com>
2025-06-05 18:34:52 +0300

    feat(events collection): add shortId, createdAt fields

--- src/schema/events.ts
+++ src/schema/events.ts
@@ -4,18 +4,29 @@ import { Timestamp, GeoPoint } from "@firebase/firestore";
 
 export const eventSchema: z.ZodType = z.object({
   title: z.string(),
-  activity: z.string().describe('TODO: must be a enum'),
+  shortId: z.string(),
+  activity: z.string().describe("TODO: must be a enum"),
   description: z.string().optional(),
   announcements: z.array(z.string()).optional(),
   capacity: z.number().optional(),
   creator: baseUserSchema,
-  deadline: z.instanceof(Timestamp).describe('fundraising deadline. if that date is reached and the goal is not met, the fundraising will be cancelled'),
+  deadline: z
+    .instanceof(Timestamp)
+    .describe(
+      "fundraising deadline. if that date is reached and the goal is not met, the fundraising will be cancelled",
+    ),
   endDate: z.instanceof(Timestamp),
-  fundraising: z.object({
-    accumulated: z.number(),
-    goal: z.number(),
-  }).optional().describe("exclusively for fundraising experience"),
-  ticketPrice: z.number().optional().describe("exclusively for ticketed experience"),
+  fundraising: z
+    .object({
+      accumulated: z.number(),
+      goal: z.number(),
+    })
+    .optional()
+    .describe("exclusively for fundraising experience"),
+  ticketPrice: z
+    .number()
+    .optional()
+    .describe("exclusively for ticketed experience"),
   imageUri: z.string(),
   link: z.string().optional(),
   location: z.object({
@@ -28,22 +39,26 @@ export const eventSchema: z.ZodType = z.object({
     shareableUsers: z.array(z.string()).optional(),
   }),
   startDate: z.instanceof(Timestamp),
-  usersChecked: z.number().describe('for ticketing experiences').optional(),
+  usersChecked: z.number().describe("for ticketing experiences").optional(),
   viewCount: z.number(),
-})
+  createdAt: z.instanceof(Timestamp)
+});
 
 export const eventAgentsSchema = z.object({
-  role: z.enum(['collaborator', 'scanner']),
-  status: z.enum(['pending', 'accepted', 'rejected']),
+  role: z.enum(["collaborator", "scanner"]),
+  status: z.enum(["pending", "accepted", "rejected"]),
   user: baseUserSchema,
-})
+});
 
 export const eventJoinersSchema = z.object({
-  phoneNumber: z.string().optional().describe('for users without the application'),
-  status: z.enum(['going', 'rejected', 'invited']),
+  phoneNumber: z
+    .string()
+    .optional()
+    .describe("for users without the application"),
+  status: z.enum(["going", "rejected", "invited"]),
   user: baseUserSchema.optional(),
-})
+});
 
-export type EventType = z.infer<typeof eventSchema>
-export type EventAgentsType = z.infer<typeof eventAgentsSchema>
-export type EventJoinersType = z.infer<typeof eventJoinersSchema>
+export type EventType = z.infer<typeof eventSchema>;
+export type EventAgentsType = z.infer<typeof eventAgentsSchema>;
+export type EventJoinersType = z.infer<typeof eventJoinersSchema>;
