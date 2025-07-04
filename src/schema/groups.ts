b537d71e6be642253bc215c2f16ca6db32474a7d
Munkhsaikhan Purevdagva <munkhp@gmail.com>
2025-07-04 17:20:25 +0800

    add last message to group

--- src/schema/groups.ts
+++ src/schema/groups.ts
@@ -2,19 +2,20 @@ import { Timestamp } from "@firebase/firestore";
 import { z } from "zod";
 import { userEssentialSchema } from "./users";
 
-export const groupSchema = z.object({
-  event: z.string(), // TODO: mark event ref
-  name: z.string().optional(),
-  type: z.enum(["people", "activities"]),
-  members: z.array(userEssentialSchema),
-}).describe("for 'people' type, the image should be taken from the avatar of the other participant; " +
-  "for 'activities' type, the image should be taken from the image of an event")
-
 export const groupMessagesSchema = z.object({
   message: z.string(),
   senderId: userEssentialSchema,
   createdAt: z.union([z.instanceof(Timestamp), z.instanceof(Date)]),
 })
+
+export const groupSchema = z.object({
+  event: z.string(), // TODO: mark event ref
+  name: z.string().optional(),
+  type: z.enum(["people", "activities"]),
+  members: z.array(userEssentialSchema),
+  lastMessage: groupMessagesSchema,
+}).describe("for 'people' type, the image should be taken from the avatar of the other participant; " +
+  "for 'activities' type, the image should be taken from the image of an event")
 
 export type GroupType = z.infer<typeof groupSchema>
 export type GroupMessagesType = z.infer<typeof groupMessagesSchema>
